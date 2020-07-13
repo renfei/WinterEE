46d72f385304fcb730fd9dbcc2e4a9601c14128a
RenFei <i@renfei.net>
2020-07-13 22:59:08 +0800

    feat: UAA服务接口

---
+++ WinterEE-API/src/main/java/com/winteree/api/service/WintereeUaaService.java
@@ -0,0 +1,25 @@
+package com.winteree.api.service;
+
+import net.renfei.sdk.entity.APIResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * <p>Title: WintereeUaaService</p>
+ * <p>Description: UAA服务</p>
+ *
+ * @author RenFei
+ * @date : 2020-07-13 16:35
+ */
+public interface WintereeUaaService {
+    @PostMapping("/oauth/token")
+    APIResult signIn(
+            @RequestParam("name") String name,
+            @RequestParam("password") String password,
+            @RequestParam("keyid") String keyid,
+            @RequestParam("grant_type") String grant_type,
+            @RequestParam("client_id") String client_id,
+            @RequestParam("client_secret") String client_secret,
+            @RequestParam("language") String language
+    );
+}
